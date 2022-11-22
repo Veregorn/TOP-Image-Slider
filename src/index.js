dc11109d423f1bf7b3a9827ed9d32f7df22f57b7
Raul Jimenez <rjbarco@gmail.com>
2022-11-22 11:52:10 +0100

    Repeat all the slides for infinite loop effect

--- src/index.js
+++ src/index.js
@@ -33,16 +33,31 @@ function createElementWithId(tag, id) {
   const img2 = new Image();
   const img3 = new Image();
   const img4 = new Image();
+  // We need to repeat the images for the effect desired
+  const img5 = new Image();
+  const img6 = new Image();
+  const img7 = new Image();
+  const img8 = new Image();
   
   img1.src = Books;
   img2.src = Tiles;
   img3.src = Purple;
   img4.src = Wall;
+  // We need to repeat the images for the effect desired
+  img5.src = Books;
+  img6.src = Tiles;
+  img7.src = Purple;
+  img8.src = Wall;
 
   const slide1 = createElementWithClass("div","slide");
   const slide2 = createElementWithClass("div","slide");
   const slide3 = createElementWithClass("div","slide");
   const slide4 = createElementWithClass("div","slide");
+  // We need to repeat the slides for the effect desired
+  const slide5 = createElementWithClass("div","slide");
+  const slide6 = createElementWithClass("div","slide");
+  const slide7 = createElementWithClass("div","slide");
+  const slide8 = createElementWithClass("div","slide");
 
   // Place elements all inside others
 
@@ -50,11 +65,19 @@ function createElementWithId(tag, id) {
   slide2.appendChild(img2);
   slide3.appendChild(img3);
   slide4.appendChild(img4);
+  slide5.appendChild(img5);
+  slide6.appendChild(img6);
+  slide7.appendChild(img7);
+  slide8.appendChild(img8);
 
   slider.appendChild(slide1);
   slider.appendChild(slide2);
   slider.appendChild(slide3);
   slider.appendChild(slide4);
+  slider.appendChild(slide5);
+  slider.appendChild(slide6);
+  slider.appendChild(slide7);
+  slider.appendChild(slide8);
 
   container.appendChild(slider);
 
